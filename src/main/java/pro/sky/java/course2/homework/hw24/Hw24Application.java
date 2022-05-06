5522771b7632aeb11d00a1d9097ac110082cbd07
nohero_ <serega1552@yandex.ru>
2022-05-06 11:47:07 +0400

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/homework/hw24/Hw24Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.homework.hw24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw24Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/homework/hw24/Hw24ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.homework.hw24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
